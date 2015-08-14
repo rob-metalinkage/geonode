5a47e9c8ffd3304aaf44752ea207cb6afc67fbc2
Clarence Davis Jr <clarence.davis@tech.geocent.com>
2015-08-14 11:30:50 -0500

    updated map config to include map owner.

--- geonode/utils.py
+++ geonode/utils.py
@@ -296,7 +296,8 @@ class GXPMapBase(object):
             'id': self.id,
             'about': {
                 'title': self.title,
-                'abstract': self.abstract
+                'abstract': self.abstract,
+                'owner': self.poc.name_long if hasattr(self,"poc") else ''
             },
             'defaultSourceType': "gxp_wmscsource",
             'sources': sources,
